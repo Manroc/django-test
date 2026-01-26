ebeaef67f0e64190ff13cfbb68cb1d27bfeb18d5
Manroc <Manroc>
2026-01-26 20:32:41 +0545

    bug fix

--- manishecom/urls.py
+++ manishecom/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from polls import views
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Hiiii from Manish🙋‍♂️</h1>")
 
 urlpatterns = [
+     path("", home),
     path('admin/', admin.site.urls),
     path("polls/", views.index, name="index"),
 ]
